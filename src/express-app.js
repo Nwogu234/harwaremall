048260799b3033a3b5e385a74a5c1a6f17b5f8f0
chucky-golden <chukschibyke92@gmail.com>
2024-01-18 05:39:11 +0100

    compression

--- src/express-app.js
+++ src/express-app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const basicRoutes = require('./routes/basicRoutes')
 const productRoutes = require('./routes/productRoutes')
 const vendorRoutes = require('./routes/vendorRoutes')
@@ -20,9 +19,7 @@ module.exports = async (app) => {
     
     app.use(cors())
     
-    app.use(bodyParser.json({ limit: '20mb' }));
-
-    app.use(express.json())
+    app.use(express.json({ limit: '10mb' }));
     app.use(express.urlencoded({ extended: true }))    
 
     // setting d express session middleware
